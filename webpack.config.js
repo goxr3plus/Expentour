9e16cd4dced064eea05a8d4cc94a0b6184cefc07
Kentros Alexandros <goxr3plus>
2019-12-30 17:34:23 +0200

    Webpack Config 1

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,48 @@
 const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  mode: 'development', //mode [development,production,...]
-  devtool: 'cheap-module-eval-source-map', // show original source code so we don't get lost inside bundle.js when errors happen
-  entry: './src/app.js', //which file to use as entry
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js' //final file name
-  },
-  //Customize the behaviour of webpack when it loads a specific file
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader' //loader allows as to give only one entry
-      },
-      {
-        test: /\.s?css$/, // accept both css and scss
-        use: [
-          //use allows as to give an array of entries
-          'style-loader', // creates style nodes from JS strings
-          'css-loader', // translates CSS into CommonJS
-          'sass-loader' // compiles Sass to CSS, using Node Sass by default
-        ]
-      }
-    ]
-  },
+module.exports = (env, ergs) => {
+  const isProduction = env === 'production'
 
-  // use dev server
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    watchContentBase: true, //Live Reload on file change
-    compress: false, //compress bundle.js
-    port: 9000, //final application running port [localhost:9000]
-    historyApiFallback: true //We gonna handle routing from client side
+  return {
+    // mode: 'development', //mode [development,production,...]
+    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map', // show original source code so we don't get lost inside bundle.js when errors happen
+    entry: './src/app.js', //which file to use as entry
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js' //final file name
+    },
+
+    //Customize the behaviour of webpack when it loads a specific file
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader' //loader allows as to give only one entry
+        },
+        {
+          test: /\.s?css$/, // accept both css and scss
+          use: [
+            MiniCssExtractPlugin.loader,
+            //use allows as to give an array of entries
+            'style-loader', // creates style nodes from JS strings
+            'css-loader', // translates CSS into CommonJS
+            'sass-loader' // compiles Sass to CSS, using Node Sass by default
+          ]
+        }
+      ]
+    },
+    //Plugins that have access to change and work with your existing
+    // Webpack build
+    plugins: [new MiniCssExtractPlugin()],
+    // use dev server
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      watchContentBase: true, //Live Reload on file change
+      compress: false, //compress bundle.js
+      port: 9000, //final application running port [localhost:9000]
+      historyApiFallback: true //We gonna handle routing from client side
+    }
   }
 }
